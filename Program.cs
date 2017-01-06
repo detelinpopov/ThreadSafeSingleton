13859d14cbcbb9b9a0867dd75cce566c132c6de7
detelinpopov <detelinpopov@yahoo.com>
2017-01-06 09:56:46 +0100

    Added project to source control

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ThreadSafeSingleton
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Singleton singleton = Singleton.Instance;
+		}
+	}
+}
---
+++ Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ThreadSafeSingleton
+{
+	/// <summary>
+	///     Thread-safe Singleton using Lazy
+	/// </summary>
+	public sealed class Singleton
+	{
+		private static readonly Lazy<Singleton> instanceHolder = new Lazy<Singleton>(() => new Singleton());
+
+		private Singleton()
+		{}
+
+		public static Singleton Instance
+		{
+			get { return instanceHolder.Value; }
+		}
+	}
+}
